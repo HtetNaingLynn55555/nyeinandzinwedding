54b74b68ea5ce5246885d4dfa7e52a1919c356d3
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-10-23 20:09:00 +0700

    change responsive countdown

--- src/components/pages/users/components/Countdown.jsx
+++ src/components/pages/users/components/Countdown.jsx
@@ -1,6 +1,6 @@
 export default function Countdown({ time }) {
   return (
-    <div className=" font-primary text-2xl flex justify-center items-center rounded-full w-15 h-15 font-bold border-2  border-[#E6A817]">
+    <div className=" font-primary text-[#16px] md:text-2xl flex justify-center items-center rounded-full md:w-15 md:h-15 w-10 h-10 font-bold border md:border-2  border-[#E6A817]">
       {time}
     </div>
   );
